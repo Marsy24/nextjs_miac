841150649005c86f9b58ae79ae503eb3fc48f720
Marsy24 <marsydev23@gmail.com>
2024-08-13 21:12:50 +0500

    fixed error deploy

--- src/shared/ui/navigation-menu.tsx
+++ src/shared/ui/navigation-menu.tsx
@@ -43,10 +43,13 @@ const NavigationMenuItem = NavigationMenuPrimitive.Item
 const navigationMenuTriggerStyle = cva(
   "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
 )
+interface IProps {
+  showIcon?: boolean;
+}
 
 const NavigationMenuTrigger = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Trigger>
+  React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Trigger> & IProps
 >(({ className, children, showIcon = true, ...props }, ref) => (
   <NavigationMenuPrimitive.Trigger
     ref={ref}
@@ -61,8 +64,9 @@ const NavigationMenuTrigger = React.forwardRef<
       />
     )}
   </NavigationMenuPrimitive.Trigger>
-))
-NavigationMenuTrigger.displayName = NavigationMenuPrimitive.Trigger.displayName
+));
+
+NavigationMenuTrigger.displayName = NavigationMenuPrimitive.Trigger.displayName;
 
 const NavigationMenuContent = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Content>,
